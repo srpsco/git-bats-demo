4dc9a78a300c0b0d66e56f3d8590cfd60ef0112b
Steve Robillard <steverobillard@gmail.com>
2020-04-28 04:12:48 -0400

    completed - all tests passing

---
+++ count_successes.sh
@@ -0,0 +1,13 @@
+
+File=$1
+
+mkdir ./test_data/files
+
+tar xfz $1 -C ./test_data/files/
+
+s=$(grep SUCCESS ./test_data/files/files/* | wc -l)
+f=$(grep FAILURE ./test_data/files/files/* | wc -l)
+
+echo "There were "$s" successes and "$f" failures."
+
+rm -rf ./test_data/files
